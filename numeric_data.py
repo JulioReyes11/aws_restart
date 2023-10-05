d7be94ae63b973df906618a5a243cb8489d6a94c
Julio Reyes <82628141+JulioReyes11@users.noreply.github.com>
2023-10-04 19:41:29 -0600

    first commit

---
+++ numeric_data.py
@@ -0,0 +1,30 @@
+# Exercise 2
+print("Python has tree numeric types: int, float, and complex")
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Exercise 3
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Exercise 4
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Exercise 5.1
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Exercise 5.2
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
